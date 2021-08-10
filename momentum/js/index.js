9b93dda577570ec2650eb39e91939a2e098d6426
Liliya <liliyavoloshine@gmail.com>
2021-08-10 11:02:43 +0400

    refactor: add async loading for bg image

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -2,8 +2,14 @@ import showTime from './time.js'
 import showDate from './date.js'
 import {showGreeting} from './greetingDaytime.js'
 import { setUsernameToStorage, getUsernameFromStorage } from './greetingName.js'
-import setBg from './slider.js'
+import {getSlideNext, getSlidePrev, setBg} from './slider.js'
 
+const slidePrev = document.querySelector('#slidePrev')
+const slideNext = document.querySelector('#slideNext')
+
+
+slidePrev.addEventListener('click', getSlidePrev)
+slideNext.addEventListener('click', getSlideNext)
 window.addEventListener('beforeunload', setUsernameToStorage)
 window.addEventListener('load', getUsernameFromStorage)
 
--- momentum/js/slider.js
+++ momentum/js/slider.js
@@ -1,15 +1,33 @@
 import { getTimeOfDay } from './greetingDaytime.js'
 
+let randomNum = getRandomNum()
+
 function getRandomNum() {
   return Math.floor(Math.random() * 20)
 }
 
-function bgNum() {
-  return getRandomNum().toString().padStart(2, 0)
+function padNum(num) {
+  return num.toString().padStart(2, 0)
 }
 
 function setBg() {
-  document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${getTimeOfDay()}/${bgNum()}.jpg')`
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${getTimeOfDay()}/${padNum(randomNum)}.jpg`
+  img.addEventListener('load', () => {      
+    document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${getTimeOfDay()}/${padNum(randomNum)}.jpg')`
+  })
 }
 
-export default setBg
+function getSlidePrev() {
+  randomNum != 1 ? randomNum-- : randomNum = 20
+  console.log(randomNum, 'getSlidePrev()')
+  setBg()
+}
+
+function getSlideNext() {
+  randomNum != 20 ? randomNum++ : randomNum = 1
+  console.log(randomNum, 'getSlideNext')
+  setBg()
+}
+
+export {getSlidePrev, getSlideNext, setBg}
