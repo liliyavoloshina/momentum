6674b5df98c6680806789913097f63b20208e7b1
Liliya <liliyavoloshine@gmail.com>
2021-08-11 10:41:46 +0400

    feat: add audios and playlist

---
+++ momentum/data/playlist.js
@@ -0,0 +1,16 @@
+const playlist = [
+  {      
+    title: "Chill Vibes",
+    src: "../audio/chill-vibes.mp3"
+  },
+  {      
+    title: "Chillout",
+    src: "../audio/chillout.mp3"
+  },
+  {      
+    title: "Easy Guitar",
+    src: "../audio/easy-guitar.mp3"
+  }
+]
+
+export default playlist
