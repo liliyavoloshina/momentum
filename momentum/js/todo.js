251bb773ebd1418e1999ab443573b6b5304f9da0
Liliya <liliyavoloshine@gmail.com>
2021-08-18 11:57:30 +0400

    feat: add style for todo adding

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -1,7 +1,12 @@
+import { changeInputColor } from './helpers.js'
 const todoOpenBtn = document.querySelector('#todoBtn')
 const todoArea = document.querySelector('#todoArea')
 const todoNameSelects = document.querySelectorAll('input[data-todolist]')
 const todolists = document.querySelectorAll('ul[data-todolist]')
+const todoNewInputs = document.querySelectorAll('.todolist-new')
+
+
+let currentTodolistName = 'inbox'
 
 function toggleTodoArea() {
   todoOpenBtn.classList.toggle('revert')
@@ -9,10 +14,11 @@ function toggleTodoArea() {
 }
 
 function changeTodolist() {
-  const todolistName = this.dataset.todolist
-  const checked = this.checked
-  todolists.forEach(todolist => todolist.dataset.todolist === todolistName ? todolist.classList.remove('hidden') : todolist.classList.add('hidden'))
+  currentTodolistName = this.dataset.todolist
+  todolists.forEach(todolist => todolist.dataset.todolist === currentTodolistName ? todolist.classList.remove('hidden') : todolist.classList.add('hidden'))
 }
 
 todoOpenBtn.addEventListener('click', toggleTodoArea)
 todoNameSelects.forEach(select => select.addEventListener('change', changeTodolist))
+todoNewInputs.forEach(input => input.addEventListener('focus', e => changeInputColor(e)))
+todoNewInputs.forEach(input => input.addEventListener('blur', e => changeInputColor(e)))
