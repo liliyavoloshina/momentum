88d6b626e7b57cb2a0eaf4e624e151db6a637086
Liliya <liliyavoloshine@gmail.com>
2021-10-15 20:43:53 +0400

    feat: add some adaptive

--- momentum/js/greetingName.js
+++ momentum/js/greetingName.js
@@ -4,26 +4,26 @@ import { changeInputColor } from './helpers.js'
 const nameEl = document.querySelector('#greetingName')
 
 function setUsernameToStorage() {
-  localStorage.setItem(
-    'username',
-    document.querySelector('#greetingName').value
-  )
+  localStorage.setItem('username', document.querySelector('#greetingName').value)
 }
 
 function getUsernameFromStorage() {
-  const lang = getLangFromStorage()
-  if (localStorage.getItem('username')) {
-    if (localStorage.getItem('username') == 'stranger' || localStorage.getItem('username') == 'незнакомец') {
-      document.querySelector('#greetingName').value =
-        lang == 'ru' ? 'незнакомец' : 'stranger'
-    } else {
-      document.querySelector('#greetingName').value =
-        localStorage.getItem('username')
-    }
-  } else {
-    document.querySelector('#greetingName').value =
-      lang == 'ru' ? 'незнакомец' : 'stranger'
+  const username = localStorage.getItem('username')
+  if (username) {
+    nameEl.value = username
   }
+  // if (localStorage.getItem('username')) {
+  //   if (localStorage.getItem('username') == 'stranger' || localStorage.getItem('username') == 'незнакомец') {
+  //     document.querySelector('#greetingName').value =
+  //       lang == 'ru' ? 'незнакомец' : 'stranger'
+  //   } else {
+  //     document.querySelector('#greetingName').value =
+  //       localStorage.getItem('username')
+  //   }
+  // } else {
+  //   document.querySelector('#greetingName').value =
+  //     lang == 'ru' ? 'незнакомец' : 'stranger'
+  // }
 }
 
 nameEl.addEventListener('focus', e => changeInputColor(e))
