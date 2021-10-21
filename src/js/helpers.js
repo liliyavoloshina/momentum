8e4a10bd43e578c791859d42d2270afa4c592686
Liliya <liliyavoloshine@gmail.com>
2021-10-21 11:23:38 +0400

    feat: add preloading for images and audio

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -16,5 +16,28 @@ function getLangFromStorage() {
   }
 }
 
+const preloadedImages = new Array()
+const images = ['../img/unmute.png', '../img/mute.png', '../img/play-audio.png', '../img/pause-audio.png']
+
+function preload(images) {
+  for (let i = 0; i < images.length; i++) {
+    preloadedImages[i] = new Image()
+    preloadedImages[i].src = images[i]
+  }
+}
+
+preload(images)
+
+const audioFiles = ['./audio/chill-vibes.mp3', './audio/chillout.mp3', './audio/easy-guitar.mp3']
+
+function preloadAudio(audioFiles) {
+  for (let i = 0; i < audioFiles.length; i++) {
+    const audio = new Audio()
+    audio.src = audioFiles[i]
+    audio.load()
+  }
+}
+
+preloadAudio(audioFiles)
 
 export { changeInputColor, getLangFromStorage }
